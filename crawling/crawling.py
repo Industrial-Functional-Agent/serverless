53c40ee3f824ef6b93406eab2cac799a6bc356de
bellatoris <bellatoris@snu.ac.kr>
2017-11-29 16:12:15 +0900

    crawling success in aws lambda

--- crawling/crawling.py
+++ crawling/crawling.py
@@ -4,16 +4,11 @@ import time
 from selenium import webdriver
 
 
-def crawling():
-    if True:
-        a = os.popen('ldd -v {}'.format(os.path.join(os.getcwd(), 'headless-chromium'))).read()
-        return a
-    start = time.time()
-    chrome_path = os.path.join(os.getcwd(), 'chromedriver-mac')
-    # phantom_path = os.path.join(home, 'practice', 'phantomjs-2.1.1-macosx/bin/phantomjs')
+def load_chrome(chromedriver, chrome):
+    chrome_path = os.path.join(os.getcwd(), chromedriver)
     # Chrome의 경우 | 아까 받은 chromedriver의 위치를 지정해준다.
     options = webdriver.ChromeOptions()
-    options.binary_location = os.path.join(os.getcwd(), 'headless-chromium')
+    options.binary_location = os.path.join(os.getcwd(), chrome)
     # options.binary_location = '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
     options.add_argument('headless')
     options.add_argument('window-size=1920x1080')
@@ -21,14 +16,20 @@ def crawling():
     options.add_argument('single-process')
     options.add_argument('no-sandbox')
     driver = webdriver.Chrome(chrome_path, chrome_options=options)
+    return driver
 
+
+def crawling():
+    start = time.time()
+    phantom_path = os.path.join(os.getcwd(), 'phantomjs-2.1.1-linux-x86_64/bin/phantomjs')
     # driver.implicitly_wait(3)
     # PhantomJS의 경우 | 아까 받은 PhantomJS의 위치를 지정해준다.
-    # driver = webdriver.PhantomJS(phantom_path)
+    driver = webdriver.PhantomJS(phantom_path,
+                                 service_log_path=os.path.devnull)
 
     driver.get('http://cafe.naver.com/joonggonara.cafe')
     # 맥북/노트북 탭으로 들어감
-    driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+    # driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
     driver.find_element_by_id('menuLink334').click()
     script = """
     window.myFunc = function() {
@@ -46,6 +47,10 @@ def crawling():
     print('total time is {}'.format(time.time() - start))
 
     return text
+
+
+def post_process(text):
+    text.split('\n')
 
 
 if __name__ == '__main__':
--- crawling/zip_for_lambda.sh
+++ crawling/zip_for_lambda.sh
@@ -1,10 +1,15 @@
 export ZIP_FILE='JoongoToSlack.zip'
 export PYTHON_VERSION='python3.6'
 export VIRTUALENV='venv_lambda'
+export PHANTOM='phantomjs-2.1.1-linux-x86_64'
+export PHANTOM_ZIP='phantomjs-2.1.1-linux-x86_64.tar.bz2'
 
 # Clean up
 rm -rf $VIRTUALENV
 rm $ZIP_FILE
+rm -rf $PHANTOM
+
+tar -xjvf $PHANTOM_ZIP
 
 # Setup fresh virtualenv and install requirements
 virtualenv --python=$PYTHON_VERSION --no-site-packages $VIRTUALENV
@@ -18,5 +23,6 @@ zip -r9 ../../../../$ZIP_FILE *
 cd ../../../../
 zip -g $ZIP_FILE main.py
 zip -g $ZIP_FILE crawling.py
-zip -g $ZIP_FILE headless-chromium
-zip -g $ZIP_FILE chromedriver
+# zip -g $ZIP_FILE headless-chromium
+# zip -g $ZIP_FILE chromedriver
+zip -g -r9 $ZIP_FILE $PHANTOM
